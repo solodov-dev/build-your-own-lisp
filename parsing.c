d1dd85f3f0ccbda194bbdab53eabce994b080987
Andrey Solodov <solodov.dev@gmail.com>
2025-12-19 17:38:36 +0300

    Chapter 8

--- parsing.c
+++ parsing.c
@@ -1,4 +1,5 @@
 #include "mpc.h"
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -25,7 +26,61 @@ void add_history(char *unused) {}
 #include <editline/readline.h>
 #endif
 
-long eval(mpc_ast_t *t);
+typedef struct {
+  int type;
+  long num;
+  int err;
+} lval;
+
+lval eval(mpc_ast_t *t);
+
+// Enum of possible lval types
+enum { LVAL_NUM, LVAL_ERR };
+
+// Enum of possible error types
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM, LERR_NOT_ENOUGH_OPERANDS };
+
+// Create a new number type lval
+lval lval_num(long x) {
+  lval v;
+  v.type = LVAL_NUM;
+  v.num = x;
+  return v;
+}
+
+lval lval_err(int x) {
+  lval v;
+  v.type = LVAL_ERR;
+  v.err = x;
+  return v;
+}
+
+void lval_print(lval v) {
+  switch (v.type) {
+  case LVAL_NUM:
+    printf("%li", v.num);
+    break;
+  case LVAL_ERR:
+    if (v.err == LERR_DIV_ZERO) {
+      printf("Error: Division By Zero");
+    }
+    if (v.err == LERR_BAD_OP) {
+      printf("Error: Invalid Operator!");
+    }
+    if (v.err == LERR_BAD_NUM) {
+      printf("Error: Invalid Number!");
+    }
+    if (v.err == LERR_NOT_ENOUGH_OPERANDS) {
+      printf("Error: Invalid Number Of Opertands!");
+    }
+    break;
+  }
+}
+
+void lval_println(lval v) {
+  lval_print(v);
+  putchar('\n');
+}
 
 int main(int argc, char **argv) {
   // Create Some Parsers
@@ -57,8 +112,8 @@ int main(int argc, char **argv) {
     // Attempt to parse the user input
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      long result = eval(r.output);
-      printf("%li\n", result);
+      lval result = eval(r.output);
+      lval_println(result);
       mpc_ast_delete(r.output);
     } else {
       // Print error
@@ -75,53 +130,62 @@ int main(int argc, char **argv) {
   return 0;
 }
 
-long eval_op(long x, char *op, long y) {
+lval eval_op(lval x, char *op, lval y) {
+  if (x.type == LVAL_ERR) {
+    return x;
+  }
+  if (y.type == LVAL_ERR) {
+    return y;
+  }
+
   if (strcmp(op, "+") == 0) {
-    return x + y;
+    return lval_num(x.num + y.num);
   }
   if (strcmp(op, "-") == 0) {
-    return x - y;
+    return lval_num(x.num - y.num);
   }
   if (strcmp(op, "*") == 0) {
-    return x * y;
+    return lval_num(x.num * y.num);
   }
   if (strcmp(op, "/") == 0) {
-    return x / y;
+    return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
   }
   if (strcmp(op, "%") == 0) {
-    return x % y;
+    return lval_num(x.num % y.num);
   }
   if (strcmp(op, "^") == 0) {
-    return x ^ y;
+    return lval_num(x.num ^ y.num);
   }
   if (strcmp(op, "max") == 0) {
-    return x > y ? x : y;
+    return x.num > y.num ? x : y;
   }
   if (strcmp(op, "min") == 0) {
-    return x < y ? x : y;
+    return x.num < y.num ? x : y;
   }
-  return 0;
+  return lval_err(LERR_BAD_OP);
 }
 
-long eval_unary_op(long x, char *op) {
+lval eval_unary_op(lval x, char *op) {
   if (strcmp(op, "-") == 0) {
-    return -x;
+    return lval_num(-x.num);
   }
 
-  return x;
+  return lval_err(LERR_NOT_ENOUGH_OPERANDS);
 }
 
-long eval(mpc_ast_t *t) {
-  // If tagged as number return it directly
+lval eval(mpc_ast_t *t) {
   if (strstr(t->tag, "number")) {
-    return atoi(t->contents);
+    // Check if there is some error in conversion
+    errno = 0;
+    long x = strtol(t->contents, NULL, 10);
+    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
   }
 
   // The operator is always second child
   char *op = t->children[1]->contents;
 
   // We store the third child in `x`
-  long x = eval(t->children[2]);
+  lval x = eval(t->children[2]);
 
   // Iterate the remaining children and combining
   int i = 3;
